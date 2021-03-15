daa6ceab445f9c8b4ab27881ee877ba1f3331a68
nataliarvboas <nataliarvboas@gmail.com>
2021-03-15 10:40:34 -0300

    creating app layout

--- app/src/main/java/com/example/cardview/ContactAdapter.kt
+++ app/src/main/java/com/example/cardview/ContactAdapter.kt
@@ -3,6 +3,8 @@ package com.example.cardview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -22,9 +24,21 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
         holder.bind(list[position])
     }
 
-    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(contact: Contact) {
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
 
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val tvPhotograph: ImageView = itemView.findViewById(R.id.iv_photograph)
+
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+//            tvPhotograph.text = contact.photograph
         }
     }
 }
--- app/src/main/java/com/example/cardview/MainActivity.kt
+++ app/src/main/java/com/example/cardview/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.cardview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+
+    }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact("Natalia Ramos", "(82) 9999-3595", "img.png"),
+                Contact("Natalia Ramos", "(82) 9999-3595", "img.png")
+            )
+        )
     }
 }
